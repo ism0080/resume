6ef91d5e97cf14d958cde69d0a27254edd909bb9
Isaac Mackle <isaac.mackle@gmail.com>
2020-02-23 11:53:17 +1000

    chore: adding eslint

--- jest.config.js
+++ jest.config.js
@@ -4,15 +4,9 @@ module.exports = {
   moduleDirectories: ['node_modules', 'src'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   moduleNameMapper: {
-    // components: '<rootDir>/src/components',
-    // scenes: '<rootDir>/src/scenes/',
-    // hooks: '<rootDir>/src/hooks',
-    // res: '<rootDir>/src/res',
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': 'test-file-stub',
     '\\.(css|less)$': 'identity-obj-proxy',
   },
-  //   testPathIgnorePatterns: ['<rootDir>/src/res/.*'],
-  //   coveragePathIgnorePatterns: ['.*.interface.ts', 'index.ts'],
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
   },
