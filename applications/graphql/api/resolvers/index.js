9e0026bb426cf60ab4723e5a31e3cdc1452e39fc
Isaac Mackle <isaac.mackle@dominos.com.au>
2021-02-17 18:42:19 +1300

    chore: update graph

--- applications/graphql/api/resolvers/index.js
+++ applications/graphql/api/resolvers/index.js
@@ -19,7 +19,7 @@ const projects = [
   {
     imageUrl: 'https://s3-ap-southeast-2.amazonaws.com/graph.isaacmackle.com-images/lyric-project.png',
     imageAlt: 'Lyrc: Random Song Lyric',
-    title: 'Lyrc &bull; Random Song Lyric',
+    title: 'Lyrc • Random Song Lyric',
     description: 'Simple use of Genius-API to search for an artist and return a random song lyric',
     websiteUrl: 'https://lyric.mackle.im',
     sourceCodeUrl: 'https://github.com/ism0080/random-song-lyric',
@@ -28,7 +28,7 @@ const projects = [
   {
     imageUrl: 'https://s3-ap-southeast-2.amazonaws.com/graph.isaacmackle.com-images/comic-project.png',
     imageAlt: 'Comic Strip Readme',
-    title: 'Comic Strip Readme &bull; GitHub Action',
+    title: 'Comic Strip Readme • GitHub Action',
     description: 'This GitHub Workflow updates your readme with the latest XKCD comic',
     websiteUrl: 'https://github.com/marketplace/actions/comic-strip-readme',
     sourceCodeUrl: 'https://github.com/ism0080/comicstrip-readme',
